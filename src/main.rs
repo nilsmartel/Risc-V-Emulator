f573386020dc9f4b648552663bde8221744d40cb
Nils Martel <nilsmartel@yahoo.de>
2026-04-13 14:38:06 +0200

    Include register definition

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+mod register;
+
+use std::default;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[derive(Default, Debug)]
+struct CPU {
+    registers: [u32; 32],
+    pc: u32,
+}
+
+impl CPU {
+    fn write_reg(&mut self, index: impl Into<usize>, value: u32) {
+        let index: usize = index.into();
+
+        // the 0 register is always 0
+        if index == 0 {
+            return;
+        }
+
+        self.registers[index] = value;
+    }
+}
